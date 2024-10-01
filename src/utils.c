df1d37ed96c3c7146065879a372bcc7de217ae96
lythx <lythxxx@gmail.com>
2024-10-01 23:22:55 +0200

    min max and binary search utils

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,5 @@
 #include "utils.h"
 #include "sys/time.h"
-#include "stdlib.h"
 
 long long time_ms(void) {
   struct timeval tv;
@@ -14,4 +13,35 @@ double rand_0_1(void) {
 
 double rand_min_max(double min, double max) {
   return min + ((double) rand() / RAND_MAX) * (max - min);
+}
+
+double min_d(double a, double b) {
+  return a < b ? a : b;
+}
+
+double max_d(double a, double b) {
+  return a > b ? a : b;
+}
+
+double min_i(int a, int b) {
+  return a < b ? a : b;
+}
+
+double max_i(int a, int b) {
+  return a > b ? a : b;
+}
+
+int binary_search(const void* val, const void* array, size_t count, size_t size,
+                  int (*comparator)(const void*, const void*)) {
+  const char* arr = (const char*) array;
+  size_t left = 0, mid, right = count;
+  while (left < right) {
+    mid = left + (right - left) / 2;
+    if (comparator(val, &arr[mid * size]) == 1) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,23 @@
 #ifndef BLUE_MAX_UTILS_H
 #define BLUE_MAX_UTILS_H
 
+#include "stdlib.h"
+
 long long time_ms(void);
 
 double rand_0_1(void);
 
 double rand_min_max(double min, double max);
+
+double min_d(double a, double b);
+
+double max_d(double a, double b);
+
+double min_i(int a, int b);
+
+double max_i(int a, int b);
+
+int binary_search(const void* val, const void* array, size_t count, size_t size,
+                  int (*comparator)(const void*, const void*));
 
 #endif //BLUE_MAX_UTILS_H
